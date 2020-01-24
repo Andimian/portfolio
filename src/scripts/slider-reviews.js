c2f1acb02e3d12d26b5dfa6a1a2a5cad55d83809
Anton Sokolov <zvuk45@bk.ru>
2020-01-24 11:41:49 +0500

    сделан слайдер осталось пододвинуть

--- src/scripts/slider-reviews.js
+++ src/scripts/slider-reviews.js
@@ -7,52 +7,54 @@ new Vue({
 	components: {
 		Flickity
 	},
-	data() {
-		return {
-			desktop: window.matchMedia("(min-width: 768px)").matches,
-			flickityOptions: {
-				prevNextButtons: false,
-				pageDots: false,
-        cellClass: 'reviews__item',
-				wrapAround: false,
-				groupCells: 1
-			}
-		};
-	},
-	methods: {
-		next() {
-			this.$refs.flickity.next();
-		},
+	// data() {
+	// 	return {
+	// 		desktop: window.matchMedia("(min-width: 768px)").matches,
+	// 		flickityOptions: {
+	// 			prevNextButtons: false,
+	// 			"autoPlay": 5000,
+	// 			pageDots: false,
+  //       cellClass: 'reviews__item',
+	// 			wrapAround: true,
+	// 			groupCells: 1
+	// 		}
+	// 	};
+	// },
+	// methods: {
+	// 	next() {
+	// 		this.$refs.flickity.next();
+	// 	},
 
-		previous() {
-			this.$refs.flickity.previous();
-		},
-		setFlickityGroupCells() {
-			this.flickityOptions.groupCells = this.desktop == true ? 2 : 1;
-		},
-	},
-	created() {
-		this.setFlickityGroupCells();
-  }
-  // data() {
-  //   return {
-  //     flickityOptions: {
-  //       initialIndex: 3,
-  //       prevNextButtons: false,
-  //       pageDots: false,
-  //       wrapAround: true
-        
-  //       // any options from Flickity can be used
-  //     }
-  //   }
-  // },
-  
-  // methods: {
-  //   next() {
-  //     this.$refs.flickity.next();
-  //   },
-  //   previous() {
-  //     this.$refs.flickity.previous();
-  //   }
+	// 	previous() {
+	// 		this.$refs.flickity.previous();
+	// 	},
+	// 	setFlickityGroupCells() {
+	// 		this.flickityOptions.groupCells = this.desktop == true ? 2 : 1;
+	// 	},
+	// },
+	// created() {
+	// 	this.setFlickityGroupCells();
   // }
+  data() {
+    return {
+      flickityOptions: {
+        initialIndex: 0,
+        prevNextButtons: false,
+        pageDots: false,
+				wrapAround: true,
+				groupCells: true,
+				autoPlay: 5000,
+        // any options from Flickity can be used
+      }
+    }
+  },
+  
+  methods: {
+    next() {
+      this.$refs.flickity.next();
+    },
+    previous() {
+      this.$refs.flickity.previous();
+    }
+  }
 });
