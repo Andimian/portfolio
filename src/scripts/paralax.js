733ea7596c2538e903d2bc2d9ec8e3eca0a65f71
Sokolov <zvuk45@bk.ru>
2020-01-26 01:33:58 +0500

    сделан слайдер, сделан паралакс, сделаны скиллы без анимации - остался адаптив, состояние фокуса на элементах и теги у кнопок и ссылок

--- src/scripts/paralax.js
+++ src/scripts/paralax.js
@@ -1,17 +1,81 @@
+      // паралакс по движению мышки
 import Parallax from 'parallax-js';
 
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene, {
-  // calibrateX: false,
+  // calibrateX: true,
   // calibrateY: true,
-  // invertX: false,
+  // dataHoverOnly: true,
+  // dataClipRelativeInput: true,
+  // invertX: true,
   // invertY: true,
-  // limitX: false,
-  // limitY: 10,
+  // limitX: 0,
+  // limitY: 0,
+  // precision: 10,
   // scalarX: 2,
   // scalarY: 8,
-  // frictionX: 0.8,
-  // frictionY: 0.8,
-  // originX: 1.0,
-  // originY: 1.0
-  });
+  clipRelativeInput: true,
+  hoverOnly: true,
+  // frictionX: 0.1,
+  // frictionY: 0.1,
+  // originX: 0.5,
+  // originY: 0.5
+});
+
+// parallaxInstance.disable();
+
+
+      // паралакс по скроллу
+var parallax = (function() {
+  var parLayer1 = document.querySelector('.paralax__layer--1')
+  var parLayer2 = document.querySelector('.paralax__layer--2')
+  var parLayer3 = document.querySelector('.paralax__layer--3')
+  var parLayer4 = document.querySelector('.paralax__layer--4')
+  var parLayer5 = document.querySelector('.paralax__layer--5')
+  
+  return {
+    move: function(block, windowScroll,strafeAmount) {
+      var strafe = windowScroll / -strafeAmount + '%';
+      var style = block.style;
+      var transformString = 'translate3d(0, ' +strafe+', 0)';
+      
+      style.top = strafe;
+      style.webkitTransform = transformString;
+    },
+    init: function(wScroll) {
+      // var для нижнего паралакса = getoffsets('start-section').top,
+      var startOffSet = getOffsets ('start-section').bottom;
+
+      if(startOffSet > 0) {
+        
+        // this.move(parLayer1, wScroll, 500);
+        this.move(parLayer2, wScroll, 120);
+        this.move(parLayer3, wScroll, 100);
+        this.move(parLayer4, wScroll, 90);
+        // this.move(parLayer5, wScroll, 50);
+      }
+
+      // if(ниж пар > 0) {
+      //   this.move(parLayer1, wScroll, 350);
+      //   this.move(parLayer1, wScroll, 350);
+      //   this.move(parLayer1, wScroll, 350);
+      //   this.move(parLayer1, wScroll, 350);
+      //   this.move(parLayer1, wScroll, 350);
+      // }
+    }
+  }
+}());
+
+function getOffsets(block) {
+  var element = document.querySelector('.' + block),
+      rect = element.getBoundingClientRect();
+  return rect;
+}
+
+// вот это запускается при скролле
+window.onscroll = function() {
+  var wScroll = window.pageYOffset;
+  parallax.init(wScroll);
+  
+
+}
--- src/scripts/slider-reviews.js
+++ src/scripts/slider-reviews.js
@@ -7,34 +7,7 @@ new Vue({
 	components: {
 		Flickity
 	},
-	// data() {
-	// 	return {
-	// 		desktop: window.matchMedia("(min-width: 768px)").matches,
-	// 		flickityOptions: {
-	// 			prevNextButtons: false,
-	// 			"autoPlay": 5000,
-	// 			pageDots: false,
-  //       cellClass: 'reviews__item',
-	// 			wrapAround: true,
-	// 			groupCells: 1
-	// 		}
-	// 	};
-	// },
-	// methods: {
-	// 	next() {
-	// 		this.$refs.flickity.next();
-	// 	},
 
-	// 	previous() {
-	// 		this.$refs.flickity.previous();
-	// 	},
-	// 	setFlickityGroupCells() {
-	// 		this.flickityOptions.groupCells = this.desktop == true ? 2 : 1;
-	// 	},
-	// },
-	// created() {
-	// 	this.setFlickityGroupCells();
-  // }
   data() {
     return {
       flickityOptions: {
@@ -43,6 +16,8 @@ new Vue({
         pageDots: false,
 				wrapAround: false,
 				groupCells: true,
+				freeScroll: false,
+				contain: true
 				// autoPlay: 6000,
       }
     }
@@ -50,10 +25,38 @@ new Vue({
   
   methods: {
     next() {
-      this.$refs.flickity.next();
+			this.$refs.flickity.next();
+			this.checkArrows();
     },
     previous() {
-      this.$refs.flickity.previous();
-    }
-  }
+			this.$refs.flickity.previous();
+			this.checkArrows();
+		},
+
+	// 	setFlickityGroupCells() {
+	// 		this.flickityOptions.groupCells = this.desktop == true ? 2 : 1;
+	// },
+	// created() {
+	// 	this.setFlickityGroupCells();
+	// }
+		checkArrows() {
+			var prev = this.$el.querySelector('.reviews__arrows-item--left');
+			var next = this.$el.querySelector('.reviews__arrows-item--right');
+			
+			
+			if (this.$refs.flickity.selectedIndex() == 0){
+				prev.disabled = true;
+				next.disabled = false;
+
+			} else if (this.$refs.flickity.selectedIndex() == this.$refs.flickity.slides().length -1){
+				// console.log(this.$refs.flickity.slides().length);
+
+				this.$el.querySelector('.reviews__arrows-item--right').disabled = true;
+				this.$el.querySelector('.reviews__arrows-item--left').disabled = false;
+			} else {
+				this.$el.querySelector('.reviews__arrows-item--right').disabled = false;
+				this.$el.querySelector('.reviews__arrows-item--left').disabled = false;
+			}
+		}
+	}
 });
