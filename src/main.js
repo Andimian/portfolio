0e073b96b52a3765cc3dc3b6d8bf3c4f0c5836a2
Anton Sokolov <zvuk45@bk.ru>
2020-01-29 16:15:52 +0500

    поправил ховер в навигации на мобилках

--- src/main.js
+++ src/main.js
@@ -6,5 +6,5 @@ if (process.env.NODE_ENV === "development") {
 import "./scripts/skills";
 import "./scripts/paralax";
 import "./scripts/slider-reviews";
-import "./scripts/test-vue";
+import "./scripts/slider-projects";
 import "./scripts/popup";
